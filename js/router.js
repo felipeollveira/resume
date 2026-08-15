91a19aecc430d1eeff478e21d317d2d4413fcdc6
felipeollveira <masfelipe241@gmail.com>
2026-08-15 16:16:09 -0300

    rota de links

--- js/router.js
+++ js/router.js
@@ -1,24 +1,34 @@
 function show(el) { if (el) el.style.display = ''; }
 function hide(el) { if (el) el.style.display = 'none'; }
 
-const contato = () => document.querySelector('.contato');
-const footer  = () => document.querySelector('footer');
+const sel = id => document.getElementById(id);
+const qs  = cls => document.querySelector(cls);
 
 const routes = {
   '/': () => {
-    show(document.querySelector('.hero'));
-    show(document.querySelector('.portfolio'));
-    hide(document.getElementById('store'));
-    show(contato());
-    show(footer());
+    show(qs('.hero'));
+    show(qs('.portfolio'));
+    hide(sel('store'));
+    hide(sel('links'));
+    show(qs('.contato'));
+    show(qs('footer'));
   },
   '/store': () => {
-    hide(document.querySelector('.hero'));
-    hide(document.querySelector('.portfolio'));
-    show(document.getElementById('store'));
-    hide(contato());
-    hide(footer());
+    hide(qs('.hero'));
+    hide(qs('.portfolio'));
+    show(sel('store'));
+    hide(sel('links'));
+    hide(qs('.contato'));
+    hide(qs('footer'));
     if (typeof fetchStoreProducts === 'function') fetchStoreProducts();
+  },
+  '/links': () => {
+    hide(qs('.hero'));
+    hide(qs('.portfolio'));
+    hide(sel('store'));
+    show(sel('links'));
+    hide(qs('.contato'));
+    hide(qs('footer'));
   },
 };
 
